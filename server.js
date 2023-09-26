9db5dc7fb5c2950d261e91ba20901585a67b67f9
ayushtripathi280 <ayushtripathi282002@gmail.com>
2023-09-26 07:07:47 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,35 @@
+const WebSocket = require('ws');
+const http = require('http');
+const express = require('express');
+
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+
+  ws.on('message', (message) => {
+    // Broadcast the received message to all connected clients
+    wss.clients.forEach((client) => {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
+  });
+
+  ws.on('close', () => {
+    console.log('Client disconnected');
+  });
+});
+
+// Serve the HTML file
+app.use(express.static(__dirname));
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+server.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
+
